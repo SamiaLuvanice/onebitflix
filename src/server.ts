79e72ec743ac06beeadadff9fec2f4c7d2ecc13e
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2026-05-07 13:01:06 -0300

    feat: create adminpage with AdminJS

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import { sequelize } from './database';
+import AdminJS from 'adminjs';
+import { adminJs, adminJsRouter } from './adminjs';
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(express.static('public'));
 
-app.listen(port, () => {
+app.use(adminJs.options.rootPath, adminJsRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   sequelize.authenticate().then(() => {
     console.log('Database connection has been established successfully.');
   })
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${PORT}`);
 });
