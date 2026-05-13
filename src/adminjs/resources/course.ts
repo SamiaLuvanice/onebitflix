e8734e74a2fa7228b3e5873a2140926353609861
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2026-05-13 15:51:06 -0300

    feat: upload of course files

--- src/adminjs/resources/course.ts
+++ src/adminjs/resources/course.ts
@@ -1,4 +1,6 @@
-import { ResourceOptions } from "adminjs";
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path from "path/win32";
 
 export const courseResourceOptions: ResourceOptions = {
     navigation: "Catálogo",
@@ -7,3 +9,18 @@ export const courseResourceOptions: ResourceOptions = {
     listProperties: ["id", "name", "featured", "categoryId"],
     showProperties: ["id", "name", "synopsis", "featured", "thumbnailUrl", "categoryId", "createdAt", "updatedAt"]
 }
+
+export const courseResourceFeature: FeatureType[] = [
+    uploadFileFeature({
+        provider: {
+            local: {
+                bucket: path.join(__dirname, '..', '..', '..', 'public')
+            }
+        },
+        properties: {
+            key: 'thumbnailUrl',
+            file: 'uploadThumbnail'
+        },
+        uploadPath: (record, filename) => `thumbnail/courses/${record.get('id')}/${filename}`
+    })
+]
